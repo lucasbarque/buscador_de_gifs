5a44d9d3cacd6f06760eaf116c70c5b858a027be
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2020-05-19 18:34:46 -0400

    Subindo Projeto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscadordegifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscadordegifs/ui/home_page.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white)
+  ));
+}
